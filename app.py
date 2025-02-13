cb7aed2469e63cd6818c1e3838060864e51c5aa4
John Rearden <reardenjohn@gmail.com>
2025-02-13 00:31:16 +0000

    Create ML pipeline to engineer data and build model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 def main():
     st.title("Welcome to Streamlit")
     st.write("This is a simple Streamlit app which just shows a welcome message.")
-    
+
+
 if __name__ == "__main__":
-    
     main()
