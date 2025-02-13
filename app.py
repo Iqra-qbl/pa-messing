153a4d632ecdcbd20dfd8906fe5c5412b090129a
John Rearden <reardenjohn@gmail.com>
2025-02-13 10:07:44 +0000

    Add correlation heatmap to streamlit app

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import streamlit as st
 
 
 def main():
-    st.title("Welcome to Streamlit")
-    st.write("This is a simple Streamlit app which just shows a welcome message.")
+    st.title("Cybersecurity Threat Detection")
+    st.write("This is a simple app which uses Streamlit to create a web app for cybersecurity threat detection.")
+    
+    st.write("Here's the correlation heatmap of the dataset:")
+    
+    # Display an image from the filesystem
+    st.image("images/heatmap.png", width=700)
 
 
 if __name__ == "__main__":
